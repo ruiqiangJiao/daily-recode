8e4bdb6eeeb96780b6aed65eb093265443c3b6dd
jiaoruiqiang <jiaoruiqiang@klicen.com>
2022-04-12 16:33:31 +0800

    add hive read

--- docs/site_libs/quarto-search/quarto-search.js
+++ docs/site_libs/quarto-search/quarto-search.js
@@ -17,12 +17,14 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   const { autocomplete } = window["@algolia/autocomplete-js"];
 
   let quartoSearchOptions = {};
+  let language = {};
   const searchOptionEl = window.document.getElementById(
     "quarto-search-options"
   );
   if (searchOptionEl) {
     const jsonStr = searchOptionEl.textContent;
     quartoSearchOptions = JSON.parse(jsonStr);
+    language = quartoSearchOptions.language;
   }
 
   // note the search mode
@@ -99,6 +101,11 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
     classNames: {
       form: "d-flex",
     },
+    translations: {
+      clearButtonTitle: language["search-clear-button-title"],
+      detachedCancelButtonText: language["search-detached-cancel-button-title"],
+      submitButtonTitle: language["search-submit-button-title"],
+    },
     initialState: {
       query,
     },
@@ -172,10 +179,10 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
                   reshapedItems.push({
                     target,
                     title: isExpanded
-                      ? `Hide additional matches`
+                      ? language["search-hide-matches-text"]
                       : remainingCount === 1
-                      ? `${remainingCount} more match in this document`
-                      : `${remainingCount} more matches in this document`,
+                      ? `${remainingCount} ${language["search-more-match-text"]}`
+                      : `${remainingCount} ${language["search-more-matches-text"]}`,
                     type: kItemTypeMore,
                     href: kItemTypeMoreHref,
                   });
@@ -287,7 +294,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
               return createElement(
                 "div",
                 { class: "quarto-search-no-results" },
-                "No results."
+                language["search-no-results-text"]
               );
             },
             header({ items, createElement }) {
@@ -300,7 +307,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
                 return createElement(
                   "div",
                   { class: "search-result-header" },
-                  `${count} matching documents.`
+                  `${count} ${language["search-matching-documents-text"]}`
                 );
               } else {
                 return createElement(
@@ -310,6 +317,26 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
                 );
               }
             },
+            footer({ _items, createElement }) {
+              if (
+                quartoSearchOptions.algolia &&
+                quartoSearchOptions.algolia["show-logo"]
+              ) {
+                const libDir = quartoSearchOptions.algolia["libDir"];
+                const logo = createElement("img", {
+                  src: offsetURL(
+                    `${libDir}/quarto-search/search-by-algolia.svg`
+                  ),
+                  class: "algolia-search-logo",
+                });
+                return createElement(
+                  "a",
+                  { href: "http://www.algolia.com/" },
+                  logo
+                );
+              }
+            },
+
             item({ item, createElement }) {
               return renderItem(
                 item,
@@ -325,6 +352,17 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
       ];
     },
   });
+
+  // Remove the labeleledby attribute since it is pointing
+  // to a non-existent label
+  if (quartoSearchOptions.type === "overlay") {
+    const inputEl = window.document.querySelector(
+      "#quarto-search .aa-Autocomplete"
+    );
+    if (inputEl) {
+      inputEl.removeAttribute("aria-labelledby");
+    }
+  }
 
   // If the main document scrolls dismiss the search results
   // (otherwise, since they're floating in the document they can scroll with the document)
@@ -453,13 +491,13 @@ function validateItems(items) {
   if (items.length > 0) {
     const item = items[0];
     const missingFields = [];
-    if (!item.href) {
+    if (item.href == undefined) {
       missingFields.push("href");
     }
-    if (!item.title) {
+    if (!item.title == undefined) {
       missingFields.push("title");
     }
-    if (!item.text) {
+    if (!item.text == undefined) {
       missingFields.push("text");
     }
 
@@ -485,6 +523,7 @@ function validateItems(items) {
 
 let lastQuery = null;
 function showCopyLink(query, options) {
+  const language = options.language;
   lastQuery = query;
   // Insert share icon
   const inputSuffixEl = window.document.body.querySelector(
@@ -500,7 +539,7 @@ function showCopyLink(query, options) {
       copyButtonEl = window.document.createElement("button");
       copyButtonEl.setAttribute("class", "aa-CopyButton");
       copyButtonEl.setAttribute("type", "button");
-      copyButtonEl.setAttribute("title", "Copy link to search");
+      copyButtonEl.setAttribute("title", language["search-copy-link-title"]);
       copyButtonEl.onmousedown = (e) => {
         e.preventDefault();
         e.stopPropagation();
@@ -807,10 +846,11 @@ function positionPanel(pos) {
   const inputEl = window.document.querySelector(
     "#quarto-search .aa-Autocomplete"
   );
+
   if (panelEl && inputEl) {
     panelEl.style.top = `${Math.round(panelEl.offsetTop)}px`;
     if (pos === "start") {
-      panelEl.style.left = `${Math.round(inputEl.offsetLeft)}px`;
+      panelEl.style.left = `${Math.round(inputEl.left)}px`;
     } else {
       panelEl.style.right = `${Math.round(inputEl.offsetRight)}px`;
     }
@@ -832,8 +872,10 @@ function highlightMatch(query, text) {
         text.slice(end);
       const clipStart = Math.max(start - 50, 0);
       const clipEnd = clipStart + 200;
+
       text = text.slice(clipStart, clipEnd);
-      return text.slice(text.indexOf(" ") + 1);
+
+      return text;
     } else {
       return text;
     }
